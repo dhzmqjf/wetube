515b266c70cf72e20a1a3442ab25d97ee20a22e1
admin <admin@MBP.local>
2020-12-24 22:19:03 +0900

    Nomade Code Utube Clone Example...

---
+++ controller/userController.js
@@ -0,0 +1,10 @@
+//Global
+export const join = (req, res) => res.render('Join');
+export const login = (req, res) => res.render('Login');
+export const logout = (req, res) => res.render('Logout');
+
+//User
+export const users = (req, res) => res.render('Users');
+export const userDetail = (req, res) => res.render('User Detail');
+export const editProfile = (req, res) => res.render('Edit Profile');
+export const changePassword = (req, res) => res.render('Change Password');
---
+++ controller/videoController.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => res.render('home');
+export const search = (req, res) => res.render('Search');
+
+export const videos = (req, res) => res.render('Videos');
+export const upload = (req, res) => res.render('Upload');
+export const videoDetail = (req, res) => res.render('VideoDetail');
+export const editVideo = (req, res) => res.render('EditVideo');
+export const detailVideo = (req, res) => res.render('DetailVideo');
